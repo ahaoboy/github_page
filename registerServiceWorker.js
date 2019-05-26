c34a1d1ddd4c250acccb30e6301cfe1852315562
Ace <421316546@qq.com>
2019-05-26 18:39:23 +0800

    init 2

--- registerServiceWorker.js
+++ registerServiceWorker.js
@@ -2,7 +2,7 @@
 console.log("registerServiceWorker.js");
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
-    navigator.serviceWorker.register("/sw.js").then(
+    navigator.serviceWorker.register("./sw.js").then(
       function(registration) {
         // Registration was successful
         console.log(
